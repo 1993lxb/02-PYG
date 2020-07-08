769c344effbfe9035e3fbe4a1cb7c6da88bf522f
xliu49 <1225519016@qq.com>
2020-07-08 23:08:12 +0800

    update

--- pages/category/index.js
+++ pages/category/index.js
@@ -5,14 +5,34 @@ Page({
   data: {
     leftMenuList: [],
     rightContentList: [],
-    currentIndex:0
+    currentIndex: 0
 
   },
   cates: [],
 
 
   onLoad: function (options) {
-    this.getCatesList();
+    //  this.getCatesList();
+    const cates = wx.getStorageSync("cates");
+    if (!cates) {
+      this.getCatesList();
+
+    }else{
+      if(Date.now()-cates.time>1000*10){
+        this.getCatesList();
+
+      }else{
+        this.cates=cates.data;
+        let menuList = this.cates.map(v => v.cat_name);
+      let contentList = this.cates[0].children;
+      this.setData({
+        leftMenuList: menuList,
+        rightContentList: contentList
+      })
+
+      }
+    }
+
 
   },
   getCatesList() {
@@ -20,6 +40,8 @@ Page({
       url: "https://api-hmugo-web.itheima.net/api/public/v1/categories"
     }).then(res => {
       this.cates = res.data.message;
+      wx.setStorageSync("cates", { time: Date.now(), data: this.cates });
+
       let menuList = this.cates.map(v => v.cat_name);
       let contentList = this.cates[0].children;
       this.setData({
@@ -28,6 +50,19 @@ Page({
       })
 
     })
+  },
+  handleItemTap(e) {
+    console.log(e);
+    let { index } = e.currentTarget.dataset;
+    let contentList = this.cates[index].children;
+
+    this.setData({
+      currentIndex: index,
+      rightContentList: contentList
+
+    })
+
+
   }
 
 
