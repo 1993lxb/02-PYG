2fda71761af6765dca5354560084dc7eb7ce55ff
xliu49 <1225519016@qq.com>
2020-07-24 19:22:48 +0800

    1111

--- pages/cart/index.js
+++ pages/cart/index.js
@@ -1,4 +1,6 @@
 // pages/cart/index.js
+import { getSetting, chooseAddress, openSetting } from "../../utils/asyncWx.js";
+import regeneratorRuntime from '../../lib/runtime/runtime';
 Page({
 
   /**
@@ -21,46 +23,71 @@ Page({
   onReady: function () {
 
   },
+  handleChooseAddress() {
+    // wx.getSetting({
+    //   success: (result) => {
+    //     const scopeAddress = result.authSetting["scope.address"];
+    //     if (scopeAddress === true || scopeAddress === undefined) {
+    //       wx.chooseAddress({
+    //         success: (result1) => {
+    //           console.log(result1)
+    //         }
+    //       });
+    //     }else{
+    //       wx.openSetting({
+    //         success: (result2) => {
+    //           wx.chooseAddress({
+    //             success: (result3) => {
+    //               console.log(result3)
+    //             }
+    //           });
+    //         },
+    //         fail: () => {},
+    //         complete: () => {}
+    //       });
 
-  /**
-   * Lifecycle function--Called when page show
-   */
-  onShow: function () {
+    //     }
+    //   },
+    // });
+    getSetting().then(result => {
+      const scopeAddress = result.authSetting["scope.address"];
+      if (scopeAddress === true || scopeAddress === undefined) {
+        chooseAddress().then(result1 => {
+          console.log(result1)
+        })
+      } else {
+        openSetting().then(result2 => {
+          chooseAddress().then(result3 => {
+            console.log(result3)
+          })
+        })
 
-  },
+      }
 
-  /**
-   * Lifecycle function--Called when page hide
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * Lifecycle function--Called when page unload
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * Page event handler function--Called when user drop down
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * Called when page reach bottom
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * Called when user click on the top right corner to share
-   */
-  onShareAppMessage: function () {
+    })
 
   }
+  // async handleChooseAddress() {
+  //   try {
+  //     // 1 获取 权限状态
+  //     const res1 = await getSetting();
+  //     const scopeAddress = res1.authSetting["scope.address"];
+  //     // 2 判断 权限状态
+  //     if (scopeAddress === false) {
+  //       await openSetting();
+  //     } 
+  //       let address = await chooseAddress();
+  //       console.log(address);
+      
+  //     // 4 调用获取收货地址的 api
+
+  //     //  address.all = address.provinceName + address.cityName + address.countyName + address.detailInfo;
+
+  //     // 5 存入到缓存中
+  //     //   wx.setStorageSync("address", address);
+
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // },
 })
