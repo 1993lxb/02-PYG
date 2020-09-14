1db8aea0fdf40f4507c756018bbbeec2b503f9e2
1993lxb <36787012+1993lxb@users.noreply.github.com>
2020-09-14 14:49:45 +0800

    update61-63

--- pages/user/index.js
+++ pages/user/index.js
@@ -5,62 +5,19 @@ Page({
    * Page initial data
    */
   data: {
+    userinfo:[]
 
   },
 
-  /**
-   * Lifecycle function--Called when page load
-   */
-  onLoad: function (options) {
 
-  },
-
-  /**
-   * Lifecycle function--Called when page is initially rendered
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * Lifecycle function--Called when page show
-   */
   onShow: function () {
+    const userinfo=wx.getStorageSync("userInfo");
+    this.setData({
+      userinfo
+    })
+      
 
   },
 
-  /**
-   * Lifecycle function--Called when page hide
-   */
-  onHide: function () {
 
-  },
-
-  /**
-   * Lifecycle function--Called when page unload
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * Page event handler function--Called when user drop down
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * Called when page reach bottom
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * Called when user click on the top right corner to share
-   */
-  onShareAppMessage: function () {
-
-  }
 })
