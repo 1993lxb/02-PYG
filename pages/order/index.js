1a316bec272f2f14cb89fc2767139f8f814aa382
1993lxb <36787012+1993lxb@users.noreply.github.com>
2020-10-20 15:53:28 +0800

    20201020

--- pages/order/index.js
+++ pages/order/index.js
@@ -5,6 +5,39 @@ Page({
    * Page initial data
    */
   data: {
+    tabs:[
+      {
+        id : 0,
+        value:'全部',
+        isActive:true
+      },
+      {
+        id : 1,
+        value:'待付款',
+        isActive:false
+      },
+      {
+        id : 2,
+        value:'待发货',
+        isActive:false
+      }
+      ,
+      {
+        id : 3,
+        value:'退款/退货',
+        isActive:false
+      }
+    ],
+
+  },
+  handletabsItemsChange(e){
+    console.log(e);
+    const {index}=e.detail;
+    let {tabs}=this.data;
+    tabs.forEach((v,i)=>i===index?v.isActive=true:v.isActive=false);
+    this.setData({
+      tabs
+    })
 
   },
 
